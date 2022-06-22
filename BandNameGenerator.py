1e2de3e949d77a224d28c1159f1915801422d0a2
mttwdevelops <mttwhwang.works@gmail.com>
2022-06-21 18:14:41 -0700

    created new project file for 100 Days of Code Challenge

---
+++ BandNameGenerator.py
@@ -0,0 +1,6 @@
+# Written 6/21/2022
+# Day 1 Project of 100 Days of Code
+
+city_name = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?")
+pet_name = input("What's your pet's name?")
+print(f"Your band name could be {city_name} {pet_name}. Goodbye!")
