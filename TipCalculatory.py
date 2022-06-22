667a92d2363d794ed7694455493ccf185cf97228
mttwdevelops <mttwhwang.works@gmail.com>
2022-06-22 00:02:37 -0700

    added tipcalculator.py

---
+++ TipCalculatory.py
@@ -0,0 +1,8 @@
+# Written 6/21/2022
+# Day 2 Project of 100 Days of Code
+
+bill_total = float(input("Welcome to the tip calculator.\nWhat was the total bill? "))
+num_people = int(input("How many people to split the bill? "))
+perc_tip = (float(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100) + 1
+final_amt = round((bill_total * perc_tip) / num_people , 2)
+print(f"Each person should pay: ${final_amt}")
