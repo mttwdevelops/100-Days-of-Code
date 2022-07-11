226bfe4d7c1b8ac1da0af97d4f68a7d3f531a720
mttwdevelops <mttwhwang.works@gmail.com>
2022-07-11 13:24:03 -0700

    finalized snakegame

--- snakegame_scoreboard.py
+++ snakegame_scoreboard.py
@@ -7,8 +7,9 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("snakegame_data.txt") as file:
+            self.high_score = int(file.read())
         self.score = 0
-        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("snakegame_data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
