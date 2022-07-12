7efe4dbfdb7cf47979151a7d0b8608ef648da3b1
mttwdevelops <mttwhwang.works@gmail.com>
2022-07-11 21:49:18 -0700

    added finalized mail merge

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -7,13 +7,18 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
         
+placeholder = "[name]"
 
-names_list = []
-# with open("/Input/Names/invited_names.txt") as file:
-#     print(file.readlines())
+with open("Mail Merge Project/Input/Names/invited_names.txt") as names_list:
+    names = names_list.readlines()
+    # print(names)
 
-# filex = open("/Input/Names/invited_names.txt")
-# print(filex)
-
-filex = open("test.txt")
-print(filex)
+with open("Mail Merge Project/Input/Letters/starting_letter.txt") as letter:
+    letter_contents = letter.read()
+    for name in names:
+        stripped_name = name.strip() # this works for /n, which is what we need it to work for
+        # print(stripped_name)
+        new_letter = letter_contents.replace(placeholder, stripped_name)
+        # print(new_letter)
+        with open(f"Mail Merge Project/Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter) 
